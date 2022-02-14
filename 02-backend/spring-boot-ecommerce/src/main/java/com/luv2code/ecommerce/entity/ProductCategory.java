2fa96c6dccbefe3c44218e0b6d5a691383beba90
Amsyar Omar <amsyargy@gmail.com>
2022-02-15 01:29:16 +0800

    Adding JPA entities

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -1,2 +1,29 @@
-package com.luv2code.ecommerce.entity;public class ProductCategory {
+package com.luv2code.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="product_category")
+// @Data -- known bug
+@Getter
+@Setter
+public class ProductCategory {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
 }
